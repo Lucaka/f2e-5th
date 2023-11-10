4078cd41cf203596940eee74d22480df5bbe6b84
james <james@advmeds.com>
2023-11-10 16:02:27 +0800

    chore: 修正 tailwindcss 錯誤提示 (Unknown at rule @applycss(unknownAtRules) )

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /* eslint-env node */
-
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+  theme: {
+    colors: {
+      primary: '#1D3260',
+      secondary: '#ECF5FC'
+    },
+    extend: {}
+  },
+  plugins: []
+}
